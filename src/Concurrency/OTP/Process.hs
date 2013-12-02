b2b649a68c07dccd5cdede1330332349dce66b6c
Ilya Rezvov <irezvov@spotware.com>
2013-12-02 16:43:05 +0200

    add terminate function

--- src/Concurrency/OTP/Process.hs
+++ src/Concurrency/OTP/Process.hs
@@ -4,11 +4,14 @@ module Concurrency.OTP.Process (
   send,
   receive,
   self,
-  exit
+  exit,
+  terminate,
+  liftIO
 ) where
 
 import Data.Maybe (isJust, fromJust)
 import Control.Concurrent (
+    ThreadId,
     forkFinally,
     myThreadId,
     killThread
@@ -27,27 +30,35 @@ import Control.Concurrent.MVar (
   )
 import Control.Monad (when)
 import Control.Monad.Reader (ReaderT(..), ask, liftIO)
+import Control.Exception.Base (SomeException(..))
 
-data Pid a = Pid (MVar (Maybe (Chan a)))
+type Queue a = MVar (Maybe (Chan a))
+data Pid a = Pid {
+    pQueue :: Queue a,
+    pTID :: ThreadId
+  }
 
 type Process a = ReaderT (Pid a) IO
 
 spawn :: Process a () -> IO (Pid a)
 spawn body = do
-  queue <- newChan
-  cell <- newMVar $ Just queue
-  let pid = Pid cell
-  forkFinally (runReaderT body pid) $ const $ putMVar cell Nothing
-  return pid
+  queue <- newChan >>= newMVar . Just
+  tid <- flip forkFinally (processFinalizer queue) $ do
+    tid <- myThreadId
+    runReaderT body (Pid queue tid)
+  return $ Pid queue tid
+
+processFinalizer :: Queue a -> Either SomeException () -> IO ()
+processFinalizer queue = const $ putMVar queue Nothing
 
 send :: Pid a -> a -> Process a ()
-send (Pid cell) msg = liftIO $ do
+send (Pid { pQueue = cell }) msg = liftIO $ do
   queue <- readMVar cell
   when (isJust queue) $ do
     writeChan (fromJust queue) msg
 
 receive :: Pid a -> Process a a
-receive (Pid cell) = liftIO $ do
+receive (Pid { pQueue = cell }) = liftIO $ do
   Just queue <- readMVar cell -- always just if we here
   readChan queue
 
@@ -58,3 +69,6 @@ exit :: Process a ()
 exit = liftIO $ do
   tId <- myThreadId
   killThread tId
+
+terminate :: Pid a -> IO ()
+terminate (Pid { pTID = tid }) = killThread tid
